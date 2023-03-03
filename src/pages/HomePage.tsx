e9a6aac03da1fe63258cf0605ec93f46edbd9470
Melake12 <wubshetmelake@gmail.com>
2023-03-03 22:27:12 +0300

    feat: home page update with new content

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 
-import { Box, Container, Card } from '@mui/material';
+import { Box, Container, Card, Stack, Button } from '@mui/material';
 import { Helmet } from 'react-helmet-async';
 import ToggleButton from '@app/components/buttons/ToggleButton';
 import OverviewWrapper from '@app/components/common/OverviewWrapper';
+import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
+  const navigate = useNavigate();
   return (
     <OverviewWrapper>
       <Helmet>
@@ -14,12 +16,19 @@ function HomePage() {
         <Box display="flex" justifyContent="center" py={5} my={10} alignItems="center">
           {/* <Logo /> */}
           <h3>Home Content</h3>
-     
+
         </Box>
         <Card sx={{ p: 10, mb: 10, borderRadius: 12 }}>
           {/* <Hero /> */}
-        <h3>Hero Content</h3>
-        <ToggleButton />
+          <Stack>
+            <h6>Simple toggle button to demonstrate unit-testing for react component.</h6>
+            <Box maxWidth={100}>
+            <ToggleButton />
+            </Box>
+
+            <h6>Simple to do list to demonstrate REST API workflow with axios</h6>
+            <Button onClick={() => navigate('/todos')}>TODO LIST</Button>
+          </Stack>
         </Card>
       </Container>
     </OverviewWrapper>
